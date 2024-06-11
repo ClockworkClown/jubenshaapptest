f7bde637faa46da3e702d7866ce68bda35df7eb8
ClockworkClown <77206657+ClockworkClown@users.noreply.github.com>
2024-06-12 02:53:05 +0800

    Update register.dart

--- frontend/lib/register.dart
+++ frontend/lib/register.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:untitled1/login_screen.dart';
+import 'package:login_screen.dart';
 import 'dart:convert';
 
 class RegistrationScreen extends StatefulWidget {
