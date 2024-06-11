a8f3aadacc109055f54971755dee427806505c57
ClockworkClown <77206657+ClockworkClown@users.noreply.github.com>
2024-06-12 03:01:14 +0800

    Update login_screen.dart

--- frontend/lib/login_screen.dart
+++ frontend/lib/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:register.dart';
-import 'package:player_homescreen.dart';
+import 'register.dart';
+import 'player_homescreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dart:convert';
